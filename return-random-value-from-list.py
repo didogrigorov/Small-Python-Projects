e85385f2e8b237ad8546bf6dce2abb78c729bbff
Dilyan Grigorov <dido.grigorov@gmail.com>
2021-12-07 02:10:24 +0200

    Update return-random-value-from-list.py

--- return-random-value-from-list.py
+++ return-random-value-from-list.py
@@ -1,10 +1,8 @@
 import random
-# Split string method
+
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 length = len(names)
 
 random_choice = random.randint(0, length - 1)
