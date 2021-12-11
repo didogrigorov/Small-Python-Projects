20a9a44de7ac5c4d459df422a00a1884eced89d4
Dilyan Grigorov <dido.grigorov@gmail.com>
2021-12-11 23:49:15 +0200

    Create largest-number-in-list.py

---
+++ largest-number-in-list.py
@@ -0,0 +1,11 @@
+numbers = input("Input a list of student scores ").split()
+for n in range(0, len(numbers)):
+  numbers[n] = int(numbers[n])
+
+#Write your code below this row 👇
+max_scores = numbers[0]
+
+for item in numbers:
+  if item > max_scores:
+      max_scores = item
+print(max_scores)
