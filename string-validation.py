c1600e92324c1647b857f9c138b4c52fc050adaf
Dilyan Grigorov <dido.grigorov@gmail.com>
2021-12-30 00:31:53 +0200

    Update string-validation.py

--- string-validation.py
+++ string-validation.py
@@ -1,48 +1,46 @@
-def fun1(s):
+def func1(s):
     for i in range(len(s)):
         if(s[i].isalnum()):
             return True;
             break;
     return False;
         
-def fun2(s):
+def func2(s):
     for i in range(len(s)):
         if(s[i].isalpha()):
             return True;
             break;
     return False;
 
-def fun3(s):
+def func3(s):
     for i in range(len(s)):
         if(s[i].isdigit()):
             return True;
             break;
     return False;
 
-def fun4(s):
+def func4(s):
     for i in range(len(s)):
         if(s[i].islower()):
             return True;
             break;
     return False; 
      
-def fun5(s):
+def func5(s):
     for i in range(len(s)):
         if(s[i].isupper()):
             return True;
             break;
     return False;
- 
-# String Validators in Python - HackerRank Solution END 
     
 if __name__ == '__main__':
     s = input()
-    flagalphanum = fun1(s)
-    alphabetical = fun2(s)
-    digits = fun3(s)
-    lowercase = fun4(s)
-    uppercase = fun5(s)
-    print(flagalphanum)
+    alphanum = func1(s)
+    alphabetical = func2(s)
+    digits = func3(s)
+    lowercase = func4(s)
+    uppercase = func5(s)
+    print(alphanum)
     print(alphabetical)
     print(digits)
     print(lowercase)
